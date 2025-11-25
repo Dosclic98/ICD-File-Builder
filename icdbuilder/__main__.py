9610a963bd0249ffbb4c224a8b08cdb0e9d02e40
dosclic98 <davidesavarro98@gmail.com>
2025-11-25 16:54:28 +0100

    Modified entry point to test the ICD buiding process

--- icdbuilder/__main__.py
+++ icdbuilder/__main__.py
@@ -1,13 +1,11 @@
-import pandapower as pp
-from pandapower.networks.cigre_networks import create_cigre_network_mv
 from icdbuilder.model.icd import ICD, ICDBuilder
-from icdbuilder.model.power import PowerModel 
+from icdbuilder.model.power import PowerModel, Split, SplitMethod
+from icdbuilder.utils import create_cigre_network_mv_all_der
 
 def main():
-    net = create_cigre_network_mv(with_der="all")
-    
-    icd = ICDBuilder.build()
-    icd.toFile("output.icd.xml")
+    net = create_cigre_network_mv_all_der()
 
     pm: PowerModel = PowerModel.fromPandapowerModel(net)
-
+    splits: list[Split] = Split.fromPowerModelSplit(pm, SplitMethod.BUS)
+    for split in splits:
+        ICDBuilder.build(split=split).toFile(f"output/{split.name}.icd.xml")
