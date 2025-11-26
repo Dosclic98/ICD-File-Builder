668c287fa46a22172c1441c8b1d63a99c5c92b20
dosclic98 <davidesavarro98@gmail.com>
2025-11-26 10:20:30 +0100

    Clearing output path before building the ICD files

--- src/icdbuilder/builder.py
+++ src/icdbuilder/builder.py
@@ -27,6 +27,11 @@ class FromPPToICDBuilder(Builder):
         splits: list[Split] = Split.fromPowerModelSplit(pm, splitMethod)
         # Create the output path directory if it does not exist
         os.makedirs(outputPath, exist_ok=True)
+        # Clear existing files in the output directory
+        for file in os.listdir(outputPath):
+            filePath = os.path.join(outputPath, file)
+            if os.path.isfile(filePath):
+                os.remove(filePath)
 
         for split in splits:
             ICDBuilder.build(split=split).toFile(outputPath.joinpath(f"{split.name}.icd.xml"))
