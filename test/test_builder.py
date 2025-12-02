d3466f93e943468103540e2e5b3a6f4296446c66
dosclic98 <davidesavarro98@gmail.com>
2025-12-02 17:17:44 +0100

    Added tests for FromPPToBinderBuilder

--- test/test_builder.py
+++ test/test_builder.py
@@ -1,10 +1,16 @@
 from icdbuilder.utils import create_cigre_network_mv_all_der
 from icdbuilder.model.power import SplitMethod
-from icdbuilder.builder import FromPPToICDBuilder
+from icdbuilder.builder import FromPPToICDBuilder, FromPPToBinderBuilder
 from pathlib import Path
 
 
 if __name__ == "__main__":
-    bldr = FromPPToICDBuilder(create_cigre_network_mv_all_der())
-    bldr.build(splitMethod=SplitMethod.BUS, outputPath=Path("output", "bus_split"))
-    bldr.build(splitMethod=SplitMethod.UNIT, outputPath=Path("output", "unit_split"))
+    net = create_cigre_network_mv_all_der()
+    bldr = FromPPToICDBuilder(net)
+    busSplits = bldr.build(splitMethod=SplitMethod.BUS, outputPath=Path("output", "bus_split"))
+    unitSplits = bldr.build(splitMethod=SplitMethod.UNIT, outputPath=Path("output", "unit_split"))
+
+    bindingsBuilder = FromPPToBinderBuilder(net)
+    bindingsBuilder.build(splitMethod=SplitMethod.BUS, outputPath=Path("output", "bus_split_bindings"))
+    bindingsBuilder.build(splitMethod=SplitMethod.UNIT, outputPath=Path("output", "unit_split_bindings"))
+    
