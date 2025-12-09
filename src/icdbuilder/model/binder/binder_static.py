d5e307059e5378a75aad8c0d4a5eb446fc5f851b
dosclic98 <davidesavarro98@gmail.com>
2025-12-09 11:34:29 +0100

    Added floating point attribute specification

--- src/icdbuilder/model/binder/binder_static.py
+++ src/icdbuilder/model/binder/binder_static.py
@@ -3,24 +3,24 @@
 # health state of the single generators)  
 
 # Active power template for a single generator measurement
-singleGenMeasTemplate = "LD_Plant$SGGMMXU{inst}$MX$TotW"
+singleGenMeasTemplate = "LD_Plant$SGGMMXU{inst}$MX$TotW$mag$f"
 # Identifier template for a single generator
-singleGenIdTemplate = "LD_Plant$SSGGDGEN{inst}$ST$GnGrId"
+singleGenIdTemplate = "LD_Plant$SSGGDGEN{inst}$ST$GnGrId$stVal"
 
 # Active power template for per-gen type generation measurement
-perGenTypeTotPTemplate = "LD_Plant${genType}MMXU1$MX$TotW"
+perGenTypeTotPTemplate = "LD_Plant${genType}MMXU1$MX$TotW$mag$f"
 # Active power template for storage measurement
-stoTotPStr = "LD_Plant$StMMXU1$MX$TotW"
+stoTotPStr = "LD_Plant$StMMXU1$MX$TotW$mag$f"
 
 # Active power template @ PdC measurement
-pdcTotPStr = "LD_Plant$PdCMMXU1$MX$TotW"
+pdcTotPStr = "LD_Plant$PdCMMXU1$MX$TotW$mag$f"
 # Reactive power template @ PdC measurement
-pdcTotQStr = "LD_Plant$PdCMMXU1$MX$TotVAr"
+pdcTotQStr = "LD_Plant$PdCMMXU1$MX$TotVAr$mag$f"
 
 # I'm kind of improperly using the following two data attributes but 
 # that's the best solution i have currently found
 # Live voltages (magnitude and angles) template @ PdC measurement for each phase
-pdcVoltMagStrs = ["LD_Plant$PdCMMXU1$MX$PPV$phsAB$cVal$mag", "LD_Plant$PdCMMXU1$MX$PPV$phsBC$cVal$mag", "LD_Plant$PdCMMXU1$MX$PPV$phsCA$cVal$mag"]
-pdcVoltAngStrs = ["LD_Plant$PdCMMXU1$MX$PPV$phsAB$cVal$ang", "LD_Plant$PdCMMXU1$MX$PPV$phsBC$cVal$ang", "LD_Plant$PdCMMXU1$MX$PPV$phsCA$cVal$ang"]
+pdcVoltMagStrs = ["LD_Plant$PdCMMXU1$MX$PPV$phsAB$cVal$mag$f", "LD_Plant$PdCMMXU1$MX$PPV$phsBC$cVal$mag$f", "LD_Plant$PdCMMXU1$MX$PPV$phsCA$cVal$mag$f"]
+pdcVoltAngStrs = ["LD_Plant$PdCMMXU1$MX$PPV$phsAB$cVal$ang$f", "LD_Plant$PdCMMXU1$MX$PPV$phsBC$cVal$ang$f", "LD_Plant$PdCMMXU1$MX$PPV$phsCA$cVal$ang$f"]
 # Line currents template @ PdC measurement array
-pdcCurrMagStrs = ["LD_Plant$PdCMMXU1$MX$A$phsA$cVal$mag", "LD_Plant$PdCMMXU1$MX$A$phsB$cVal$mag", "LD_Plant$PdCMMXU1$MX$A$phsC$cVal$mag"]
+pdcCurrMagStrs = ["LD_Plant$PdCMMXU1$MX$A$phsA$cVal$mag$f", "LD_Plant$PdCMMXU1$MX$A$phsB$cVal$mag$f", "LD_Plant$PdCMMXU1$MX$A$phsC$cVal$mag$f"]
